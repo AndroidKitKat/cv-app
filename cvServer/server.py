9b7c4d9a56e1ef5c67917cdc059dc175c7905ee3
マイケル・アイズマン <kitkat@hey.com>
2020-10-26 23:52:28 -0400

    basic server that can communicate with the app

---
+++ cvServer/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/identify", methods=["PUT"])
+@cross_origin()
+def testing2():
+    data = request.json
+    # data contains a base64 encoded JPEG of the user submitted photo
+    # do the actual CV work here
+    if data:
+        print(data)
+        
+    return "This is from a post"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8008)
