0a03c49eb6180092b4a143c1ed5bc3c050c199d9
マイケル・アイズマン <kitkat@hey.com>
2020-10-26 23:52:11 -0400

    Working 'app'

--- cvApp/src/assets/vue/pages/choose.vue
+++ cvApp/src/assets/vue/pages/choose.vue
@@ -2,13 +2,18 @@
 
 <template>
   <f7-page>
-      <f7-navbar title="Choose" back-link="Back"></f7-navbar>
-      <img style="width: 100%" v-if="imagePath !== ''" :src="imagePath" />
-      <f7-button class="button-outline-ios" @click="takePicture">{{ btnTitle }}</f7-button>
-      <f7-button class="button-outline-ios" @click="sendPicture" v-if="btnTitle === 'Choose different photo'">Submit</f7-button>
-            <f7-button class="button-outline-ios" @click="netcall" v-if="btnTitle === 'Choose different photo'">Submit</f7-button>
-
-      {{ dater }}
+    <f7-navbar title="Choose" back-link="Back"></f7-navbar>
+    <img style="width: 100%" v-if="imagePath !== ''" :src="imagePath" />
+    <f7-button class="button-outline-ios" @click="takePicture">{{
+      btnTitle
+    }}</f7-button>
+    <f7-button
+      class="button-outline-ios"
+      @click="sendPicture"
+      v-if="btnTitle === 'Choose different photo'"
+      >Submit</f7-button
+    >
+    {{ dater }}
   </f7-page>
 </template>
 <script>
@@ -20,65 +25,44 @@ export default {
     return {
       imagePath: "",
       btnTitle: "Choose photo",
-      dater: ""
+      dater: "",
+      imageBase64: "",
     };
   },
   methods: {
     takePicture: function () {
       if (navigator.camera) {
         navigator.camera.getPicture(this.setPicture, this.error, {
+          quality: 50,
           destinationType: Camera.DestinationType.DATA_URL,
+          encodingType: Camera.EncodingType.JPEG,
           sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+          mediaType: Camera.MediaType.PICTURE
         });
       } else {
         // If the navigator.camera is not available display generic error to the user.
         this.error();
       }
     },
-    setPicture: function(imageData) {
-      this.imagePath = "data:image/jpeg;base64," + imageData
-      this.btnTitle = "Choose different photo"
+    setPicture: function (imageData) {
+      this.imagePath = "data:image/jpeg;base64," + imageData;
+      this.btnTitle = "Choose different photo";
     },
-    error: function() {
+    error: function () {
       nativeAlert("this is the sad");
     },
-    sendPicture: function() {
-
-fetch('http://10.18.16.48:9090/test.txt').then(res=>res.text()).then(data=>nativeAlert(data))
-
-// 1. Create a new XMLHttpRequest object
-    // let xhr = new XMLHttpRequest();
-
-    // // 2. Configure it: GET-request for the URL /article/.../load
-    // xhr.open('GET', 'https://waifupaste.moe/raw/nOK.txt');
-
-    // // 3. Send the request over the network
-    // xhr.send();
-
-    // // 4. This will be called after the response is received
-    // xhr.onload = function() {
-    //   if (xhr.status != 200) { // analyze HTTP status of the response
-    //     nativeAlert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-    //   } else { // show the result
-    //     nativeAlert(`Done, got ${xhr.response.length} bytes`); // response is the server
-    //   }
-    // };
-
-    // xhr.onprogress = function(event) {
-    //   if (event.lengthComputable) {
-    //     nativeAlert(`Received ${event.loaded} of ${event.total} bytes`);
-    //   } else {
-    //     nativeAlert(`Received ${event.loaded} bytes`); // no Content-Length
-    //   }
-
-    // };
-
-    // xhr.onerror = function() {
-    //   this.dater = xhr
-    //   nativeAlert('poop')
-    // };
+    sendPicture: function () {
+      var body = {
+        picture: this.imagePath
+      }
+      fetch("http://10.18.16.48:8008/identify", {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body),
+      }).then(res => res.text()).then(data => this.dater = data)
     },
-  // lifecycle hooks
-  }
-}
+  },
+};
 </script>
--- cvApp/src/assets/vue/pages/identify.vue
+++ cvApp/src/assets/vue/pages/identify.vue
@@ -2,10 +2,18 @@
 
 <template>
   <f7-page>
-      <f7-navbar title="Identify" back-link="Back"></f7-navbar>
-      <img style="width: 100%" v-if="imagePath !== ''" :src="imagePath" />
-      <f7-button class="button-outline-ios" @click="takePicture">{{ btnTitle }}</f7-button>
-      <f7-button class="button-outline-ios" @click="sendPicture" v-if="btnTitle === 'Retake photo'">Submit</f7-button>
+    <f7-navbar title="Choose" back-link="Back"></f7-navbar>
+    <img style="width: 100%" v-if="imagePath !== ''" :src="imagePath" />
+    <f7-button class="button-outline-ios" @click="takePicture">{{
+      btnTitle
+    }}</f7-button>
+    <f7-button
+      class="button-outline-ios"
+      @click="sendPicture"
+      v-if="btnTitle === 'Take different photo'"
+      >Submit</f7-button
+    >
+    {{ dater }}
   </f7-page>
 </template>
 <script>
@@ -17,27 +25,44 @@ export default {
     return {
       imagePath: "",
       btnTitle: "Take photo",
+      dater: "",
+      imageBase64: "",
     };
   },
   methods: {
     takePicture: function () {
       if (navigator.camera) {
         navigator.camera.getPicture(this.setPicture, this.error, {
+          quality: 50,
           destinationType: Camera.DestinationType.DATA_URL,
+          encodingType: Camera.EncodingType.JPEG,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          mediaType: Camera.MediaType.PICTURE
         });
       } else {
         // If the navigator.camera is not available display generic error to the user.
         this.error();
       }
     },
-    setPicture(imageData) {
-      this.imagePath = "data:image/jpeg;base64," + imageData
-      this.btnTitle = "Retake photo"
+    setPicture: function (imageData) {
+      this.imagePath = "data:image/jpeg;base64," + imageData;
+      this.btnTitle = "Take different photo";
     },
-    error() {
+    error: function () {
       nativeAlert("this is the sad");
     },
+    sendPicture: function () {
+      var body = {
+        picture: this.imagePath
+      }
+      fetch("http://10.18.16.48:8008/identify", {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body),
+      }).then(res => res.text()).then(data => this.dater = data)
+    },
   },
-  // lifecycle hooks
 };
 </script>
