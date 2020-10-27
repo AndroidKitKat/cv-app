b51e378168e61b97262f3fb9fee1eab1815f35dc
マイケル・アイズマン <kitkat@hey.com>
2020-10-26 22:00:30 -0400

    Revert "upload first portion of app"
    This reverts commit 94abf65794567c31f71b11b5362914df96dfa185.

--- cvApp/src/assets/libs/index.js
+++
@@ -1,8 +0,0 @@
-export const nativeAlert = (message, title) => {
-    if(navigator.notification) {
-      navigator.notification.alert(message, () => {}, title, 'Ok');
-    }else{
-      alert(message);
-    }
-  };
-  
