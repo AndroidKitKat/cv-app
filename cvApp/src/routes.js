4ee5e669900d830e70727606df223ac712080d0b
マイケル・アイズマン <kitkat@hey.com>
2020-10-26 22:01:18 -0400

    Revert "Revert "upload first portion of app""
    This reverts commit b51e378168e61b97262f3fb9fee1eab1815f35dc.

---
+++ cvApp/src/routes.js
@@ -0,0 +1,54 @@
+import Home from './assets/vue/pages/home.vue';
+import AboutPage from './assets/vue/pages/about.vue';
+import FormPage from './assets/vue/pages/form.vue';
+import DynamicRoutePage from './assets/vue/pages/dynamic-route.vue';
+
+import PanelLeftPage from './assets/vue/pages/panel-left.vue';
+import ColorThemes from './assets/vue/pages/color-themes.vue';
+import Chat from './assets/vue/pages/chat.vue';
+import Vuex from './assets/vue/pages/vuex.vue';
+import Identify from './assets/vue/pages/identify.vue';
+import Choose from './assets/vue/pages/choose.vue';
+
+export default [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/about/',
+    component: AboutPage
+  },
+  {
+    path: '/form/',
+    component: FormPage
+  },
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    component: DynamicRoutePage
+  },
+  {
+    path: '/panel-left/',
+    component: PanelLeftPage
+  },
+  {
+    path: '/color-themes/',
+    component: ColorThemes
+  },
+  {
+    path: '/chat/',
+    component: Chat
+  },
+  {
+    path: '/vuex/',
+    component: Vuex
+  },
+  {
+    path: '/identify/',
+    component: Identify
+  },
+  {
+    path: '/choose/',
+    component: Choose
+  },
+];
---
+++ cvApp/www/platform_cordova_files/browser/cordova_plugins.js
@@ -0,0 +1,56 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/src/browser/CameraProxy.js",
+        "id": "cordova-plugin-camera.CameraProxy",
+        "pluginId": "cordova-plugin-camera",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-camera": "5.0.0",
+    "cordova-plugin-statusbar": "3.0.0-dev"
+}
+// BOTTOM OF METADATA
+});
